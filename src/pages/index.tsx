02d319cd3ecc1a10dfb3a4ba899878c19ced0ac5
podnes <podnestrade@gmail.com>
2023-03-15 18:13:06 +0200

    init version

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>AHMO chat</title>
+        <meta name="description" content="ahmo gaming chat" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <h1>Initial</h1>
+      </div>
+    </>
+  )
+}
